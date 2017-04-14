0fc4f0db6cbbc7b7169b0f2b3e92962e1b72336b
shennana <shennana@jd.com>
2017-04-14 14:25:53 +0800

    五子棋

--- static/js/index.js
+++ static/js/index.js
@@ -1,74 +1,106 @@
-/**
- * Created by shennana on 2017/3/15.
- */
-angular.module('app', ['components'])
+// /**
+//  * Created by shennana on 2017/3/15.
+//  */
+//  
+
+	window.onload = function(){
+		var curr = 'black';
+		init();	
+		function init(){
+			var tdEle = document.getElementsByTagName('td');
+			for(var i=0;i<tdEle.length;i++){
+				tdEle[i].onclick = down
+			}
+		}
+		function down(){
+		// alert("棋子落地");
+		if(this.hasEle){
+			alert("这里已经有棋子了");
+			return;
+		}
+		if(curr == 'black'){
+			this.style.background = 'url(/static/images/playB.png) -1px 0 no-repeat';
+			this.hasEle = curr == 'white'?'w':'b';
+			curr = 'white';
+		}else{
+			this.style.background = 'url(/static/images/playA.png) -1px 0 no-repeat';
+			this.hasEle = curr == 'white'?'w':'b';
+			curr = 'black';
+		}
+		
+	}
+	}
+
+
+
+// angular.module('app', ['components'])
  
-.controller('coreController', function($scope, $locale) {
-    $scope.dbs = [{
-        name:'aaa',
-        code:'1'
-    },{
-        name:'bbb',
-        code:'2'
-    },{
-        name:'ccc',
-        code:'3'
-    },{
-        name:'ddd',
-        code:'4'
-    }]
+// .controller('coreController', function($scope, $locale) {
+//     $scope.dbs = [{
+//         name:'aaa',
+//         code:'1'
+//     },{
+//         name:'bbb',
+//         code:'2'
+//     },{
+//         name:'ccc',
+//         code:'3'
+//     },{
+//         name:'ddd',
+//         code:'4'
+//     }]
 
-    $scope.personalMsg={
-      "name":"amies",
-      "email":"amies.shen@gmail.com"
-    }
-    $scope.format = "M/d/yy h:mm:ss a";
-    $scope.names = "amies-outside";
-    $scope.isHideDialog = false;
-    $scope.closeDialog = function(message){
-    	$scope.isHideDialog = true;
-    	$scope.message = message;
-    }
-})
-.controller('AdvancedDemoController', function($scope, $locale) {
+//     $scope.personalMsg={
+//       "name":"amies",
+//       "email":"amies.shen@gmail.com"
+//     }
+//     $scope.format = "M/d/yy h:mm:ss a";
+//     $scope.names = "amies-outside";
+//     $scope.isHideDialog = false;
+//     $scope.closeDialog = function(message){
+//     	$scope.isHideDialog = true;
+//     	$scope.message = message;
+//     }
+// })
+// .controller('AdvancedDemoController', function($scope, $locale) {
    
-    $scope.dragoverCallback = function(index, external, type, callback) {
-        $scope.logListEvent('dragged over', index, external, type);
-        // Invoke callback to origin for container types.
-        if (type == 'container' && !external) {
-            console.log('Container being dragged contains ' + callback() + ' items');
-        }
-        return index < 10; // Disallow dropping in the third row.
-    };
+//     $scope.dragoverCallback = function(index, external, type, callback) {
+//         $scope.logListEvent('dragged over', index, external, type);
+//         // Invoke callback to origin for container types.
+//         if (type == 'container' && !external) {
+//             console.log('Container being dragged contains ' + callback() + ' items');
+//         }
+//         return index < 10; // Disallow dropping in the third row.
+//     };
 
-    $scope.dropCallback = function(index, item, external, type) {
-        $scope.logListEvent('dropped at', index, external, type);
-        // Return false here to cancel drop. Return true if you insert the item yourself.
-        return item;
-    };
+//     $scope.dropCallback = function(index, item, external, type) {
+//         $scope.logListEvent('dropped at', index, external, type);
+//         // Return false here to cancel drop. Return true if you insert the item yourself.
+//         return item;
+//     };
 
-    $scope.logEvent = function(message) {
-        console.log(message);
-    };
+//     $scope.logEvent = function(message) {
+//         console.log(message);
+//     };
 
-    $scope.logListEvent = function(action, index, external, type) {
-        var message = external ? 'External ' : '';
-        message += type + ' element was ' + action + ' position ' + index;
-        console.log(message);
-    };
+//     $scope.logListEvent = function(action, index, external, type) {
+//         var message = external ? 'External ' : '';
+//         message += type + ' element was ' + action + ' position ' + index;
+//         console.log(message);
+//     };
 
-    // Initialize model
-    $scope.model = [[], []];
-    var id = 10;
-    angular.forEach(['all', 'move', 'copy', 'link', 'copyLink', 'copyMove'], function(effect, i) {
-      var container = {items: [], effectAllowed: effect};
-      for (var k = 0; k < 7; ++k) {
-        container.items.push({label: effect + ' ' + id++, effectAllowed: effect});
-      }
-      $scope.model[i % $scope.model.length].push(container);
-    });
+//     // Initialize model
+//     $scope.model = [[], []];
+//     var id = 10;
+//     angular.forEach(['all', 'move', 'copy', 'link', 'copyLink', 'copyMove'], function(effect, i) {
+//       var container = {items: [], effectAllowed: effect};
+//       for (var k = 0; k < 7; ++k) {
+//         container.items.push({label: effect + ' ' + id++, effectAllowed: effect});
+//       }
+//       $scope.model[i % $scope.model.length].push(container);
+//     });
 
-    $scope.$watch('model', function(model) {
-        $scope.modelAsJson = angular.toJson(model, true);
-    }, true);
-})
+//     $scope.$watch('model', function(model) {
+//         $scope.modelAsJson = angular.toJson(model, true);
+//     }, true);
+// })
